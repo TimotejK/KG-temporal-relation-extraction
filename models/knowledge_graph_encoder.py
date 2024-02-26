5fb710afdcca64bceb0637e9845ff26a88883bee
Timotej Knez <timotej.knez@fri.uni-lj.si>
2024-02-26 13:57:20 +0100

    Training graph encoder

---
+++ models/knowledge_graph_encoder.py
@@ -0,0 +1,83 @@
+import torch
+from torch import nn
+from transformers import AutoModel, AutoTokenizer
+
+import torch_geometric.nn as pyg_nn
+
+class TemporalRelationAggregation(pyg_nn.MessagePassing):
+    def __init__(self, in_channels, out_channels, edge_features):
+        super(TemporalRelationAggregation, self).__init__(aggr='add')  # "Add" aggregation.
+        self.layers = 2
+        self.out_channels = out_channels
+        self.edge_features = edge_features
+        self.lin = nn.Linear(in_channels + edge_features, out_channels, bias=True)
+        self.lin_self = nn.Linear(in_channels, out_channels, bias=True)
+
+    def forward(self, x, edge_index, edge_attr):
+        # Transform node feature matrix.
+        self_x = self.lin_self(x)
+        return self_x + self.propagate(edge_index, x=x, edge_attr=edge_attr)
+
+    def message(self, x_i, x_j, edge_attr):
+        # _i central node that collects information
+        # _j neighbour node
+        if len(x_j) > 0 and len(edge_attr) > 0:
+            return self.lin(torch.cat((x_j, edge_attr), 1))
+        else:
+            return torch.zeros(0, self.out_channels)
+
+
+class GraphEncoder(nn.Module):
+    def __init__(self, node_size=768, edge_size=768, output_size=50, number_of_relations=3, dropout=0.2):
+        super(GraphEncoder, self).__init__()
+        self.node_size = node_size
+        self.edge_size = edge_size
+        self.number_of_relations = number_of_relations
+        self.pooling_strategy = "concat_entities"
+        self.hidden_dim = output_size
+
+        self.criterion = nn.CrossEntropyLoss()
+
+        self.convs = nn.ModuleList()
+        self.convs.append(self.build_conv_model(self.node_size, self.edge_size, self.hidden_dim))
+        self.lns = nn.ModuleList()
+        self.num_layers = 2
+        for l in range(self.num_layers - 1):
+            self.lns.append(nn.LayerNorm(self.hidden_dim).float())
+            self.convs.append(self.build_conv_model(self.hidden_dim, self.edge_size, self.hidden_dim))
+        self.linear = nn.Linear(self.hidden_dim, self.hidden_dim).float()
+        self.post_mp = nn.Sequential(
+            nn.Linear(self.hidden_dim * 2, self.hidden_dim), nn.Dropout(dropout),
+            nn.LeakyReLU(),
+            nn.Linear(self.hidden_dim, number_of_relations))
+        self.dropout = dropout
+        pass
+    def build_conv_model(self, node_size, edge_size, output_node_size):
+        return TemporalRelationAggregation(node_size, output_node_size, edge_size)
+
+    def forward(self, data, labels, return_embedding=False):
+        # if not self.training:
+        #     return nn.functional.one_hot(data.rule_based_prediction).double()
+        x, edge_index, batch, event1, event2, edge_attr = data.x, data.edge_index, data.batch, data.event1_index, data.event2_index, data.edge_attr
+
+        if data.num_node_features == 0:
+            x = torch.ones(data.num_nodes, 1)
+
+        for i in range(self.num_layers):
+            x = self.convs[i](x=x.float(), edge_index=edge_index.type(torch.int64), edge_attr=edge_attr.float())
+            if not i == self.num_layers - 1:
+                x = self.lns[i](x)
+
+        x = self.linear(x)
+
+        event1_emb = x[event1.int()]
+        event2_emb = x[event2.int()]
+        x = torch.cat((event1_emb, event2_emb), 1)
+
+        if return_embedding:
+            return x
+
+        x = self.post_mp(x)
+
+        loss = self.criterion(x, labels)
+        return {"loss": loss, "predictions": x}
---
+++ training/train_graph_encoder.py
@@ -0,0 +1,115 @@
+import numpy as np
+import evaluate
+from torch_geometric.data import DataLoader
+from transformers import Trainer, TrainingArguments
+from transformers.models.graphormer.collating_graphormer import preprocess_item, GraphormerDataCollator
+
+from custom_datasets.common import split_data
+from dataLoaders.combining_data import read_i2b2
+from custom_datasets.knowledge_graph_dataset import generate_primekg_graph_for_event, create_knowledge_graph_dataset, \
+    generate_llm_graph_for_event
+from models.bimodal import MultiModalPrediction
+from models.knowledge_graph_encoder import GraphEncoder
+
+def prepare_dataset():
+    df = read_i2b2(full_text=True, use_test_files=False, include_rows_without_absolute=True)
+    df_train, df_val, df_test = split_data(df, oversample=True, label_name='class', train_size=0.7, val_size=0.2, split_by_documents=True)
+    dataset_train = create_knowledge_graph_dataset(df_train, lambda event: generate_llm_graph_for_event(event, cache_only=True))
+    dataset_val = create_knowledge_graph_dataset(df_val, lambda event: generate_llm_graph_for_event(event, cache_only=True))
+    dataset_train.pregenerate_and_filter()
+    dataset_val.pregenerate_and_filter()
+    return dataset_train, dataset_val
+
+def collate_function(examples):
+    loader = DataLoader(examples, batch_size=len(examples))
+    batch = next(iter(loader))
+    return {"data": batch, "labels": batch.y}
+
+metric = evaluate.load("accuracy")
+def compute_metrics(eval_pred):
+    logits, labels = eval_pred
+    predictions = np.argmax(logits, axis=-1)
+    return metric.compute(predictions=predictions, references=labels)
+
+def compute_objective(eval_pred):
+    return eval_pred["eval_accuracy"]
+
+def train():
+    dataset_train, dataset_val = prepare_dataset()
+
+    model = GraphEncoder(node_size=768, edge_size=768, number_of_relations=3, dropout=0.2)
+    # model = MultiModalPrediction(number_of_relations=3, combine_embeddings=True)
+
+    training_args = TrainingArguments(
+        output_dir="./results",
+        learning_rate=2e-2,
+        per_device_train_batch_size=64,
+        per_device_eval_batch_size=64,
+        auto_find_batch_size=True,
+        num_train_epochs=50,
+        weight_decay=0.01,
+        gradient_accumulation_steps=1,
+        evaluation_strategy="epoch",
+        logging_strategy="epoch",
+        push_to_hub=False
+    )
+
+    trainer = Trainer(
+        model=model,
+        args=training_args,
+        train_dataset=dataset_train,
+        eval_dataset=dataset_val,
+        data_collator=collate_function,
+        compute_metrics=compute_metrics
+    )
+    trainer.train()
+
+def hyper_parameter_search():
+    dataset_train, dataset_val = prepare_dataset()
+
+    def model_init(trial):
+        return GraphEncoder(node_size=768, edge_size=768, number_of_relations=3, dropout=0.2)
+
+    def wandb_hp_space(trial):
+        return {
+            "method": "random",
+            "metric": {"name": "objective", "goal": "maximize"},
+            "parameters": {
+                "learning_rate": {"distribution": "uniform", "min": 1e-6, "max": 1e-1},
+                "weight_decay": {"distribution": "uniform", "min": 1e-6, "max": 1e-1},
+            },
+        }
+
+    training_args = TrainingArguments(
+        output_dir="./results",
+        per_device_train_batch_size=64,
+        per_device_eval_batch_size=64,
+        auto_find_batch_size=True,
+        num_train_epochs=5,
+        gradient_accumulation_steps=1,
+        evaluation_strategy="epoch",
+        logging_strategy="epoch",
+        push_to_hub=False
+    )
+
+    trainer = Trainer(
+        model=None,
+        args=training_args,
+        train_dataset=dataset_train,
+        eval_dataset=dataset_val,
+        compute_metrics=compute_metrics,
+        model_init=model_init,
+        data_collator=collate_function,
+    )
+
+    best_trial = trainer.hyperparameter_search(
+        direction="maximize",
+        backend="wandb",
+        hp_space=wandb_hp_space,
+        n_trials=20,
+        compute_objective=compute_objective,
+    )
+    return best_trial
+
+if __name__ == '__main__':
+    hyper_parameter_search()
