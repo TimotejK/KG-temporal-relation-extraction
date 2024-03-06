2a7b4468aabf712ac20295d9649cc3121546f571
Timotej Knez <timotej.knez@fri.uni-lj.si>
2024-03-06 16:06:59 +0100

    deployment script

---
+++ deploy.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+scp main.py timotej.knez@frida:/shared/home/timotej.knez/llm-graph-construction
+rsync -av -r graph_building/local_graph timotej.knez@frida:/shared/home/timotej.knez/llm-graph-construction/graph_building
+scp -r graph_building/llm timotej.knez@frida:/shared/home/timotej.knez/llm-graph-construction/graph_building
+scp graph_building/graph_construction.py timotej.knez@frida:/shared/home/timotej.knez/llm-graph-construction/graph_building/graph_construction.py
+scp graph_building/node_embeddings.py timotej.knez@frida:/shared/home/timotej.knez/llm-graph-construction/graph_building/node_embeddings.py
+#scp -r dataset_loaders timotej.knez@frida:/shared/home/timotej.knez/llm-graph-construction
+#scp -r dataLoaders timotej.knez@frida:/shared/home/timotej.knez/llm-graph-construction
+scp -r custom_datasets timotej.knez@frida:/shared/home/timotej.knez/llm-graph-construction
+scp -r models timotej.knez@frida:/shared/home/timotej.knez/llm-graph-construction
+scp -r training timotej.knez@frida:/shared/home/timotej.knez/llm-graph-construction
